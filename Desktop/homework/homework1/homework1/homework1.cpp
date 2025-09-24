4d9260b3afcaa87972e1a7a72be48673546b0237
Ivan Volkov <ivanvolkov199y@gamil.com>
2025-09-24 18:48:47 +0500

    homework1

---
+++ Desktop/homework/homework1/homework1/homework1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::string userName;
+    getline(std::cin, userName);
+    std::cout << "Привет, " << userName << "\n";
+    return 0;
+}
