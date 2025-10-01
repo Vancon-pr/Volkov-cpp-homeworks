0211addec5314e7adced4f75505218b1b367af6f
Vancon-pr <ivanvolkov199y@gmail.com>
2025-10-01 18:19:56 +0500

    Add files via upload

---
+++ homework3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a;
+	
+	try
+	{
+		cout << "Введите  сумму в рублях: ";
+		cin >> a;
+		if (a)
+		{
+
+			cout << "В долларах: " << a/85 << "\n" << "В евро: " << a/100 << "\n";
+		}
+		else
+		{
+		throw "Необхлдимо ввести число\n";
+		}
+	}
+	catch (const char* error_messange)
+	{
+		cout << error_messange;
+	}
+	return 0;
+}
