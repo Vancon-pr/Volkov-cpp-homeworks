0ec95201b0c05729e884f4707ed073079b22150d
Vancon-pr <ivanvolkov199y@gmail.com>
2025-10-01 17:51:25 +0500

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a;
+	cout << "Введите число: ";
+	cin >> a;
+	cout << "Квадрат: " << pow(a, 2) << "\n" << "Куб: " << pow(a,3) << "\n";
+	return 0;
+}
