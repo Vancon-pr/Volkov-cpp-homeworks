fe30d4cd6a98f44d279bd09176b8cd6227c44925
Vancon-pr <ivanvolkov199y@gmail.com>
2025-10-08 18:14:50 +0500

    Add files via upload

---
+++ homework4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите символ: ";
+	char a;
+	cin >> a;
+	cout <<"Код символа: " << (int)a << '\n' << "Следующий символ: "<<(char)((int)a+1) << '\n' << "Код следующего символа: " << (int)a + 1 << '\n';
+}
