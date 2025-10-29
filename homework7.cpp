b0fc0000137fffc1d9d512418dd4008c4a1b6d4a
Vancon-pr <ivanvolkov199y@gmail.com>
2025-10-29 18:28:07 +0500

    Add files via upload

---
+++ homework7.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a;
+    cout << "Введите число: ";
+    cin >> a;
+    if (!(a < pow(10,6) and a > -pow(10,6)))
+    {
+        cout << "Число не соответствует требованиям\n";
+        return 0;
+    }
+    int b=0;
+    int i = 10;
+    int j = 0;
+    while (a % i < a)
+    {
+        j += 1;
+        i *= 10;
+    }
+    int p = a;
+    for (int i = 1; i <= j+1; i++)
+    {
+        //cout << p<<' '<<b << '\n';
+        b += p % 10*pow(10,j+1-i);
+        p = p / 10;
+        
+    }
+    if (a == b)
+    {
+        cout << "true\n";
+    }
+    else
+    {
+        cout << "false\n";
+    }
+    return 0;
+}
