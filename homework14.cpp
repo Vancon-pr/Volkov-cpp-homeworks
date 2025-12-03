c90d2841c0b73353de8b843fb8e87af1f6a75e8b
Vancon-pr <ivanvolkov199y@gmail.com>
2025-12-03 18:16:10 +0500

    Add files via upload

---
+++ homework14.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Ввведите слова\n";
+    string str;
+    int i = 0;
+    int l = 0;
+    getline(cin, str);
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            l++;
+        }
+        i++;
+    }
+    string* mass = new string[l + 1];
+    bool* m = new bool[l + 1];
+    i = 0; int j = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] != ' ')
+        {
+            mass[j] += str[i];
+        }
+        else
+        {
+            j++;
+        }
+        i++;
+    }
+    bool flag = true;
+    for (int i = 0; i < l + 1; i++)
+    {
+        if(mass[i] == "flick")
+        {
+            flag = false;
+        }
+        m[i] = flag;
+        cout << flag << ' ';
+    }
+    cout << '\n';
+    return 0;
+}
