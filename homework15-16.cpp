305b5ca2041172fd1396938f0867fdb2ed738028
Vancon-pr <ivanvolkov199y@gmail.com>
2025-12-10 17:36:19 +0500

    Add files via upload

---
+++ homework15-16.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Rus");
+    float x, y;
+    cout << "Введите координаты точки\n";
+    cin >> x >> y;
+    float fl = (x - 2) * (x - 2) + (y + 1) * (y + 1);
+    if (fl > 25)
+    {
+        cout << "Точка снаружи окружности\n";
+    }
+    else
+    {
+        if (fl == 25)
+        {
+            cout << "Точка на границе окружности\n";
+        }
+        else
+        {
+            cout << "Точка внутри окружности\n";
+        }
+    }
+    return 0;
+}
