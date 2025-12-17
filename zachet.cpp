07e46fa1b7b63bacdd936bfa6bd9bbc31e8877de
Vancon-pr <ivanvolkov199y@gmail.com>
2025-12-17 20:52:18 +0500

    Add files via upload

---
+++ zachet.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    string str;
+    int x;
+    cout << "Введите массив\n";
+    getline(cin, str);
+    cout << "Введите элемент для поиска\n";
+    cin >> x;
+    int i = 0;
+    int l = 1;
+    while (str[i] != '\0')
+    {
+        
+        if (str[i] == ',')
+        {
+            l++;
+        }
+        if (str[i] == '{' or str[i] == '}')
+        {
+            str[i] = ' ';
+        }
+        i++;
+    }
+    int* mass = new int[l];
+    string prom = "";
+    i = 0;
+    l = 0;
+    bool fl = true;
+    while (str[i] != '\0')
+    {
+        if (str[i] != ',')
+        {
+            prom += str[i];
+            i++;
+        }
+        else
+        {
+            mass[l] = stoi(prom);
+            prom = "";
+            l++;
+            i++;
+        }
+    }
+    for (int i = 0; i < l; i++)
+    {
+        if (mass[i] == x)
+        {
+            cout << "Индекс искомого элемента " << i << '\n';
+            fl = false;
+            break;
+        }
+    }
+    if (fl)
+    {
+        cout << "-1\n";
+        return -1;
+    }
+    return 0;
+}
